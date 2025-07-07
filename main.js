7a5b3e348dd09ceb5e7e3376bece225ef3c58f95
yongsoocho <yongsoocho578@gmail.com>
2025-07-07 21:24:40 +0900

    start chat room

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ import { createServer } from "node:http";
 import { UserRouter } from "./src/routes/user.router.js";
 import { errorHandler, notFoundHandler } from "./src/handler/error.handler.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
 	cors: {
-		origin: "*",
+		origin: "http://localhost:5173",
 		credentials: true,
 	},
 });
@@ -18,6 +19,12 @@ const io = new Server(httpServer, {
 async function bootstrap() {
 	const PORT = process.env.PORT || 8000;
 
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		}),
+	);
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(cookieParser());
